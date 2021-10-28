3ac70870a1a7bcf700e465ce9a18f7697063b0f2
Feavy <quentin071100@live.fr>
2021-10-28 21:22:15 +0200

    Use prod server URL when NODE_ENV isn't development.

--- src/feathers-client.ts
+++ src/feathers-client.ts
@@ -6,11 +6,11 @@ import feathers from '@feathersjs/client'
 
 const app = feathers()
 
-// const socket = io(process.env.BASE_URL)
-const socket = io() // seems like with no argument, connection is made with the server which served index.html
+// En mode dev on utilise le serveur de backend qui tourne en local, sinon on est en prod et on utilise celui de prod qui tourne sur qovery
+const socket = process.env.NODE_ENV === 'development' ? io() : io('https://zaf20d6c1-z8220ecf7-gtw.qovery.io')
+
 app.configure(feathers.socketio(socket))
 
 app.configure(feathers.authentication({ storage: window.sessionStorage }))
-
 
 export default app

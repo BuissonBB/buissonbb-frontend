b8da7f1a8d678c73090527af658b6472ae02569c
Feavy <feavy>
2021-11-10 13:05:57 +0100

    Update backend URL (moved to heroku).

--- src/feathers-client.ts
+++ src/feathers-client.ts
@@ -7,7 +7,7 @@ import feathers from '@feathersjs/client'
 const app = feathers()
 
 // En mode dev on utilise le serveur de backend qui tourne en local, sinon on est en prod et on utilise celui de prod qui tourne sur qovery
-const socket = process.env.NODE_ENV === 'development' ? io() : io('https://zaf20d6c1-z8220ecf7-gtw.qovery.io')
+const socket = process.env.NODE_ENV === 'development' ? io() : io('https://buisson-backend.herokuapp.com')
 
 app.configure(feathers.socketio(socket))
 
